89e2ef9c9e841c6922f1a2955be0cb776c5961d4
DanielKGM <atbreezy4@gmail.com>
2024-11-13 19:46:21 -0300

    feat: angular material

--- src/app/components/login-layout/login-layout.component.spec.ts
+++ src/app/components/login-layout/login-layout.component.spec.ts
@@ -8,9 +8,8 @@ describe('LoginLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginLayoutComponent]
-    })
-    .compileComponents();
+      imports: [LoginLayoutComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginLayoutComponent);
     component = fixture.componentInstance;
--- src/app/components/login-layout/login-layout.component.ts
+++ src/app/components/login-layout/login-layout.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login-layout',
   standalone: true,
   imports: [],
   templateUrl: './login-layout.component.html',
-  styleUrl: './login-layout.component.scss'
+  styleUrl: './login-layout.component.scss',
 })
 export class LoginLayoutComponent {
-
+  @Input() title: string = '';
+  @Input() btnPrimarioTxt: string = '';
+  @Input() btnSecundarioTxt: string = '';
 }
